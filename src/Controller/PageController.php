744f0853387c94b52a63bcb6821a88b690034ff7
daniilkoversun <koversundanil@gmail.com>
2024-06-07 11:36:24 +0300

    Added migration to create page table

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,14 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Page;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 
 class PageController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function home(): Response
     {
+
+        $page = new Page();
+        $page->setKeywords('Любимые видео')
+            ->setDescription('Любимые видео')
+            ->setTitle('Любимые видео')
+            ->setContent('Сайт для добавления видео и удобной разметки');
+
+        $this->entityManager->persist($page);
+        $this->entityManager->flush();
+
         return $this->render('page/index.html.twig');
     }
 }
